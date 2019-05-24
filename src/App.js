71fbd132072f359a27a3fb9aa93c40c2c4498290
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-05-24 16:33:26 -0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
+    <React.Fragment>
+    <Header />
     <p>Hello World</p>
+    </React.Fragment>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { Link } from 'react-router-dom';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class MyAppBar extends React.Component {
+  state = {
+      anchorEl: null
+  };
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+  logout = event => {
+    console.log("logout");
+    firebase.auth().signOut();
+  };
+
+render() {
+    const { classes, user, login } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} onClick={this.handleMenu} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose} to="/" component={Link}>Home</MenuItem>
+                  <MenuItem onClick={this.handleClose} to="/list" component={Link}>Play</MenuItem>
+                  <MenuItem onClick={this.handleClose} to="/make" component={Link}>Make</MenuItem>
+                  <MenuItem onClick={this.handleClose} to="/about" component={Link}>About</MenuItem>
+            </Menu>            
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Tourney Games
+            </Typography>
+            {
+                (user) ?
+                <Button color="inherit" onClick={this.logout}>Logout</Button>
+                : <Button color="inherit" to={login || "/login"} component={Link}>Login</Button>
+            }
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+MyAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MyAppBar);
