ba91f388235ae8392985c18595f3d8f15b1f375a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-05-27 16:42:51 -0700

    admin init

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
