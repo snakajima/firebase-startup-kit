b15b5aed17bc68f15a74145292db9c8adfe67230
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-05-27 08:51:28 -0700

    usernames

--- src/Account.js
+++ src/Account.js
@@ -19,26 +19,63 @@ const styles = theme => ({
   },
 });
 
-const Account = props => {
-  const { classes, user } = props;
-  if (!user) {
-    return <Redirect to={"/login"} />
+class Account extends React.Component {
+  state = { name: undefined, label: "" };
+
+  componentDidMount() {
+    this.componentDidUpdate();
   }
-  return (
-    <React.Fragment>
-      <Header user={user} login="/Login/target/about" />
-      <Grid container justify="center" alignItems="center" direction="row" className={classes.root}>
-          <Grid className={classes.caption}>
-          <Typography component="h2" variant="h5" gutterBottom>
-            Account Page. 
-          </Typography>
-          <TextField label={"unique name"} value={user.displayName}
-                  className={classes.textField} margin="normal" 
-                  onChange = {(e)=>this.handleChange(e)} />
-          </Grid>
-      </Grid>
-    </React.Fragment>
-  );
+
+  componentDidUpdate() {
+    const { user } = this.props;
+
+    if (user && this.state.name===undefined) {
+      this.handleChange("");
+    }
+  }
+
+  async handleChange(name) {
+    const { db } = this.props;
+    this.setState({name:name});
+    if (name.length === 0) {
+      this.setState({label:"user name"})
+      return;
+    }
+    if (name.length < 4) {
+      this.setState({label:"Minimum 4 characters"})
+      return;
+    }
+    const ref = db.collection("usernames").doc(name);
+    const doc = await ref.get();
+    const data = doc.data();
+    if (data) {
+      this.setState({label:"Not available"})
+      return;
+    }
+    this.setState({label:"Available"})
+  }
+
+  render() {
+    const { classes, user } = this.props;
+    if (!user) {
+      return <Redirect to={"/login"} />
+    }
+    return (
+      <React.Fragment>
+        <Header user={user} login="/Login/target/about" />
+        <Grid container justify="center" alignItems="center" direction="row" className={classes.root}>
+            <Grid className={classes.caption}>
+            <Typography component="h2" variant="h5" gutterBottom>
+              Account Page. 
+            </Typography>
+            <TextField label={this.state.label} value={this.state.name || ""}
+                    className={classes.textField} margin="normal" 
+                    onChange = {(e)=>this.handleChange(e.target.value)} />
+            </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
 }
 
 Account.propTypes = {
