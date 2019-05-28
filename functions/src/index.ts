74f7ef2811fce3306905cf58cd3cbf6bb71b1e59
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-05-27 17:18:41 -0700

    express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,17 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as express from 'express';
 
 admin.initializeApp();
+
+const app = express();
+
+app.get('/entry', (request, res) => {
+    console.log("app.entry called")
+    res.send("Hello from Firebase! 2");
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.send("Hello from Firebase!");
-});
+export const entry = functions.https.onRequest(app);
