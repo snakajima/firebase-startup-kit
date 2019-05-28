552245314f2120b7a570b8fa303150e78c5631ec
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-05-28 06:09:45 -0700

    try catch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,36 +2,40 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
+import { Transaction } from '@google-cloud/firestore';
 
 admin.initializeApp();
-
 const app = express();
 app.use(cors());
 
 app.put('/api/username', async (req, res) => {
-    const db = admin.firestore();
     const { name, uid } = req.query;
-    //const info = {a:1, b:req.query.name, c:req.query.uid};
+    const db = admin.firestore();
     const ref = db.collection("usernames").doc(name);
-    const doc = await ref.get();
-    const data = doc.data();
-    if (data) {
-        if (data.uid === uid) {
-            res.json({result:"already have it"});
-        } else {
-            res.json({result:"not available"});
+
+    const operation = async (tr:Transaction) => {
+        try {
+            const doc = await tr.get(ref);
+            const data = doc.data();
+            if (data) {
+                if (data.uid === uid) {
+                    await res.json({result:"already have it"});
+                } else {
+                    await res.json({result:"not available"});
+                }
+            } else {
+                await tr.set(ref, {uid});
+                await res.json({result:"got it"});
+            }
+        } catch(err) {
+            // do nothing
         }
-    } else {
-        await ref.set({uid});
-        res.json({result:"got it"});
+    }
+    try {
+        await db.runTransaction(operation);
+    } catch(err) {
+            // do nothing
     }
 });
 
-app.put('/api/2', (request, res) => {
-    res.send("Hello from Firebase! 2");
-});
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
 export const api = functions.https.onRequest(app);
