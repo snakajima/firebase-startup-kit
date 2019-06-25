5a4cf285567bb09c456bb15b1a14bf286be1c678
ossan-engineer <bodega1123@gmail.com>
2019-06-25 19:54:10 +0900

    Convert Decoder component from class into function

--- src/Decoder.js
+++ src/Decoder.js
@@ -1,40 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
+
 import Processing from './Processing';
 
 // This module performs an 'instruction' after the login,
 // such as posting a high score (when the user play before login).
 // This is done by redirecting to '/login/cmd/{encoded}',
 // where the encoded is an encoded 'instruction' (a Json object).
-class Decoder extends React.Component {
-  state = { redirect: null };
 
-  async componentDidMount() {
-    const {
-      user,
-      db,
-      match: {
-        params: { encoded },
-      },
-    } = this.props;
-    const params = JSON.parse(decodeURIComponent(encoded));
+const Decoder = props => {
+  const [redirect, setRedirect] = useState(null);
+  const {
+    user,
+    db,
+    match: {
+      params: { encoded },
+    },
+  } = props;
+  const params = JSON.parse(decodeURIComponent(encoded));
 
+  useEffect(() => {
     // App specific code should be written here. This is just a sample.
     if (params.cmd === 'something') {
       // do something using the data given in params.
       const { foo, bar, url } = params;
       console.log(foo, bar, user, db); // to eliminate warning
       //await do.something();
-      this.setState({ redirect: url });
+      setRedirect(url);
     }
-  }
+  }, [db, params, user]);
 
-  render() {
-    if (this.state.redirect) {
-      return <Redirect to={this.state.redirect} />;
-    }
-    return <Processing user={this.props.user} />;
+  if (redirect) {
+    return <Redirect to={redirect} />;
   }
-}
+  return <Processing user={user} />;
+};
 
 export default Decoder;
